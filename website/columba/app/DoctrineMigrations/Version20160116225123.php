70c59b10db7e22745916362b8c0d275c0e9b9c4a
lorenzorapetti <lorenzo.rapetti.94@gmail.com>
2016-01-16 23:08:56 +0100

    Changed migration (again)

--- website/columba/app/DoctrineMigrations/Version20160116225123.php
+++ website/columba/app/DoctrineMigrations/Version20160116225123.php
@@ -18,14 +18,7 @@ class Version20160116225123 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE oauth_access_token ADD CONSTRAINT FK_F7FA86A419EB6921 FOREIGN KEY (client_id) REFERENCES oauth_client (id)');
-        $this->addSql('ALTER TABLE oauth_access_token ADD CONSTRAINT FK_F7FA86A4A76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id)');
-        $this->addSql('ALTER TABLE oauth_auth_code ADD CONSTRAINT FK_4D12F0E019EB6921 FOREIGN KEY (client_id) REFERENCES oauth_client (id)');
-        $this->addSql('ALTER TABLE oauth_auth_code ADD CONSTRAINT FK_4D12F0E0A76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id)');
-        $this->addSql('ALTER TABLE oauth_refresh_token ADD CONSTRAINT FK_55DCF75519EB6921 FOREIGN KEY (client_id) REFERENCES oauth_client (id)');
-        $this->addSql('ALTER TABLE oauth_refresh_token ADD CONSTRAINT FK_55DCF755A76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id)');
         $this->addSql('ALTER TABLE fos_user ADD gcm_token VARCHAR(1000) DEFAULT NULL');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_957A64796B01BC5B ON fos_user (phone_number)');
     }
 
     /**
@@ -36,10 +29,6 @@ class Version20160116225123 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE oauth_access_token DROP FOREIGN KEY FK_F7FA86A419EB6921');
-        $this->addSql('ALTER TABLE oauth_auth_code DROP FOREIGN KEY FK_4D12F0E019EB6921');
-        $this->addSql('ALTER TABLE oauth_refresh_token DROP FOREIGN KEY FK_55DCF75519EB6921');
-        $this->addSql('DROP INDEX UNIQ_957A64796B01BC5B ON fos_user');
         $this->addSql('ALTER TABLE fos_user DROP gcm_token');
     }
 }
