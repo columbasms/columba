7f6c70d2bcab0cc2e53fe67b1a7cb14b27c22624
lorenzorapetti <lorenzo.rapetti.94@gmail.com>
2016-01-30 21:09:46 +0100

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,6 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_conf,       "#{release_path}/config/puma.rb"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
