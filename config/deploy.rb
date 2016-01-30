ceaf9bff701941c54fe67fe107ff87b7852b4134
lorenzorapetti <lorenzo.rapetti.94@gmail.com>
2016-01-30 21:08:27 +0100

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,6 @@ set :repo_url, 'git@github.com:columbasms/columba.git'
 set :user,      'ubuntu'
 set :puma_threads, [4, 16]
 set :puma_workers, 0
-set :puma_conf, "#{release_path}/config/puma.rb"
 
 # Don't change these unless you know what you're doing
 set :pty,             true
@@ -21,6 +20,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_conf,       "#{release_path}/config/puma.rb"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
