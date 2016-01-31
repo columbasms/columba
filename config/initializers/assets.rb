09f7601bf6353e90fd37845c930ee458d1cf649f
lorenzorapetti <lorenzo.rapetti.94@gmail.com>
2016-01-31 18:06:21 +0100

    Added favicon

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( ie9.css windows.chrome.fix.css login.js application_login.js )
+Rails.application.config.assets.precompile += %w( ie9.css windows.chrome.fix.css login.js favicon.ico )
