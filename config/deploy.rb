cb4a79df0127efd4eb7750814cfba9eac94a4e6b
lorenzorapetti <lorenzo.rapetti.94@gmail.com>
2016-02-05 17:47:25 +0100

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :branch,        :site
 ## Linked Files & Directories (Default None):
 set :linked_files, %w{.env.production}
 # set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -74,31 +74,6 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       invoke 'puma:restart'
-    end
-  end
-
-  Rake::Task['deploy:assets:precompile'].clear_actions
-
-  namespace :assets do
-    desc 'Precompile assets locally and upload to servers'
-    task :precompile do
-      on roles(fetch(:assets_roles)) do
-        run_locally do
-          with rails_env: fetch(:rails_env) do
-            execute 'bin/rake assets:precompile'
-          end
-        end
-
-        within release_path do
-          with rails_env: fetch(:rails_env) do
-            old_manifest_path = "#{shared_path}/public/assets/manifest*"
-            execute :rm, old_manifest_path if test "[ -f #{old_manifest_path} ]"
-            upload!('./public/assets/', "#{shared_path}/public/", recursive: true)
-          end
-        end
-
-        run_locally { execute 'rm -rf public/assets' }
-      end
     end
   end
 
