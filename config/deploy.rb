4bbb24920a191553af736c8b126095e676195ac2
lorenzorapetti <lorenzo.rapetti.94@gmail.com>
2016-02-05 17:08:14 +0100

    try to solve assets problem in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,6 +50,17 @@ namespace :puma do
 end
 
 namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
@@ -77,7 +88,7 @@ namespace :deploy do
   end
 
   before :starting,     :check_revision
-  after  :finishing,    :compile_assets
+  after  :finishing,    :precompile
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
