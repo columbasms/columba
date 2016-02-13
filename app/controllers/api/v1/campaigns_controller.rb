6be583df27c395b1936fe6122c95216926581067
Alessio <alessio.cecconi.1991@gmail.com>
2016-02-13 21:16:48 +0100

    - if no campaign match the preferences of the user or the user has no preferences all the campaigns are returned

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -8,10 +8,16 @@ class Api::V1::CampaignsController < ApplicationController
 
   # GET /campaigns
   def index
-    if @user.nil?
+    # if user param is not used or the user doesn't follow any topic
+    if @user.nil? or @user.topics.empty?
       @campaigns = Campaign.not_expired
     else
       @campaigns = Campaign.not_expired.includes(:topics).where(topics: { id: @user.topics.select(:topic_id) })
+      if @campaigns.empty?
+        # if no active campaign match the user's preferences all the campaigns are returned
+        # ALESSIO: maybe in this case we should notify the user about it
+        @campaigns = Campaign.not_expired
+      end
     end
     if params[:order_field].present? and params[:order_type].present?
       @campaigns = @campaigns.order("#{params[:order_field]}": :"#{params[:order_type]}")
