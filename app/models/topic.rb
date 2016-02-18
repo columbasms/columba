5a598e0fe14aae178f7f1514943007c7f6908a9b
lorenzorapetti <lorenzo.rapetti.94@gmail.com>
2016-02-18 17:46:52 +0100

    Added topic image in API

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -9,11 +9,16 @@ class Topic < ActiveRecord::Base
   has_and_belongs_to_many :digits_clients
 
   has_attached_file :image, styles: {
-      normal: '250x250#',
+      normal: '1280x720#',
+      mobile: '800x450#',
       thumb: '32x32#'
   }, default_url: '/images/avatar.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   validates :name, presence: true
+
+  def image_mobile
+    URI.join(ActionController::Base.asset_host, self.image.url(:mobile)).to_s
+  end
 
 end
