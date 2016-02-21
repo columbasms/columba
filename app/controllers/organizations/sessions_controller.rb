05e45b5f718e3a80987c54c2796e5f64a3c9720f
lorenzorapetti <lorenzo.rapetti.94@gmail.com>
2016-02-21 22:41:23 +0100

    Redirect to edit profile at first organization login

--- app/controllers/organizations/sessions_controller.rb
+++ app/controllers/organizations/sessions_controller.rb
@@ -16,10 +16,15 @@ class Organizations::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
     yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    if self.resource.visible?
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      set_flash_message(:warning, :not_visible)
+      redirect_to edit_organization_registration_path(resource)
+    end
   end
 
   # DELETE /resource/sign_out
