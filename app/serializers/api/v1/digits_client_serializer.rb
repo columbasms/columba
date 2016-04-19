f8c7c2431bcca78f55ccba4d43491894fb4350c9
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2016-04-19 15:58:38 +0200

    Added sms_total and sms_last_month

--- app/serializers/api/v1/digits_client_serializer.rb
+++ app/serializers/api/v1/digits_client_serializer.rb
@@ -1,4 +1,13 @@
 class Api::V1::DigitsClientSerializer < ActiveModel::Serializer
   attributes :id, :user_name, :avatar_normal, :cover_normal,
-             :organizations_count, :forwarded_campaigns_count, :auth_token, :max_sms
+             :organizations_count, :forwarded_campaigns_count,
+             :auth_token, :max_sms, :sms_total, :sms_last_month
+
+  def sms_total
+    object.campaign_client_receivers.count
+  end
+
+  def sms_last_month
+    object.campaign_client_receivers.where('campaign_client_receivers.created_at > ?', Date.today - 29.days).count
+  end
 end
